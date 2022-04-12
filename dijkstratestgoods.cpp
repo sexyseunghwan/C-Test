a0c4d57edfb282f5a213caaab53ecb6c49cb7e6e
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-04-13 00:16:20 +0900

    2022-04-13
    2022-04-13

--- dijkstratestgoods.cpp
+++ dijkstratestgoods.cpp
@@ -11,13 +11,13 @@ int N,R,M;
 int CASE = 1;
 int visit[MAX];
 int prep_map[MAX];
-vector< pair<int,int> > node[MAX];
+vector< pair<int,int> > edge[MAX];
 
 void clean() {
     for (int i = 0; i < MAX; i++) {
         visit[i] = INF;
         prep_map[i] = -1;
-        vector< pair<int,int> >().swap(node[i]);
+        vector< pair<int,int> >().swap(edge[i]);
     }
 }
 
@@ -35,9 +35,9 @@ void dijkstra() {
 
         if (visit[cur] < cur_w) continue;
 
-        for (int i = 0; i < node[cur].size(); i++) {
-            int next = node[cur][i].first;//next vertex
-            int next_w = node[cur][i].second;//weight of next vertex
+        for (int i = 0; i < edge[cur].size(); i++) {
+            int next = edge[cur][i].first;//next vertex
+            int next_w = edge[cur][i].second;//weight of next vertex
 
             if (visit[next] > cur_w + next_w) {
                 visit[next] = cur_w + next_w;
@@ -47,23 +47,8 @@ void dijkstra() {
         }//for
     }//while
 
-    cout << "===================";
-    for (int i = 0; i < MAX; i++) {
-        cout << prep_map[i] << " "; //<< "\n";
-        //cout << visit[i] << "\n";
-    }
-    cout << "\n";
-
-    cout << "===================";
-    for (int i = 0; i < MAX; i++) {
-        cout << visit[i] << " "; //<< "\n";
-        //cout << visit[i] << "\n";
-    }
-    cout << "\n";
-
-
     //impossible meet supreme commissioner
-    if (visit[M-1] == INF) cout << "Case #" << CASE++ << ": -1";
+    if (visit[M-1] == INF) cout << "Case #" << CASE++ << ": -1\n";
     //possible meet supreme commissioner
     else {
         stack<int> st;
@@ -79,6 +64,8 @@ void dijkstra() {
             st.pop();
             cout << " " << num;
         }
+
+        cout << " " << M-1 << "\n";
     }
 }
 
@@ -94,8 +81,8 @@ int main()
             int start, end, weight;
 
             cin >> start >> end >> weight;
-            node[start].push_back(make_pair(end,weight));
-            node[end].push_back(make_pair(start,weight));
+            edge[start].push_back(make_pair(end,weight));
+            edge[end].push_back(make_pair(start,weight));
 
         }
 
