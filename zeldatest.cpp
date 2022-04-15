4128435ec06d953505dd13c7f9e6e522835ba5c4
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-04-15 23:56:50 +0900

    2022-04-15
    2022-04-15

--- zeldatest.cpp
+++ zeldatest.cpp
@@ -6,65 +6,68 @@
 using namespace std;
 
 int N;
-int CASE = 1;
+int Problem = 1;
 int INF = INT_MAX;
-int map[126][126];
-int check[126][126];
 int dr[4] = {0,0,-1,1};
 int dc[4] = {1,-1,0,0};
+int map[126][126];
+int visited[126][126];
 
-void clear() {
+
+void clean() {
     for (int i = 0; i < N*N; i++) {
-        check[i/N][i%N] = INF;
+        visited[i/N][i%N] = INF;
     }
 }
 
 void dijkstra() {
-    priority_queue< pair<int , pair<int,int> > > pq;
-    check[0][0] = map[0][0];
+    priority_queue< pair< int,pair<int,int> > > pq;
     pq.push(make_pair(-map[0][0],make_pair(0,0)));
+    visited[0][0] = map[0][0];
 
     while(!pq.empty()) {
-        int cur_d = -pq.top().first;
+        int cur_w = -pq.top().first;
         int cur_r = pq.top().second.first;
         int cur_c = pq.top().second.second;
 
         pq.pop();
 
         for (int i = 0; i < 4; i++) {
-            int next_r = cur_r + dr[i];
-            int next_c = cur_c + dc[i];
+            int new_r = cur_r + dr[i];
+            int new_c = cur_c + dc[i];
 
-            if (next_r >= 0 && next_c >= 0 && next_r < N && next_c < N) {
-                if (check[next_r][next_c] > cur_d + map[next_r][next_c]) {
-                    check[next_r][next_c] = cur_d + map[next_r][next_c];
-                    pq.push(make_pair(-check[next_r][next_c],make_pair(next_r,next_c)));
+            if (new_r >= 0 && new_c >= 0 && new_r < N && new_c < N) {
+                if (visited[new_r][new_c] > cur_w + map[new_r][new_c]) {
+                    visited[new_r][new_c] = cur_w + map[new_r][new_c];
+                    pq.push(make_pair(-visited[new_r][new_c],make_pair(new_r,new_c)));
                 }
-            }            
+            }
         }
-    }//while
-
-    cout << "Problem " << CASE++ << ": " << check[N-1][N-1] << "\n";
+    }
 
 }
 
 int main() 
 {
+
     while(1) {
         cin >> N;
 
         if (N == 0) break;
-        
-        clear();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                cin >> map[i][j];
-            }
+        clean();
+
+        for (int i = 0; i < N*N; i++) {
+            cin >> map[i/N][i%N];
         }
 
         dijkstra();
+        
+        cout << "Problem " << Problem++ << ": " << visited[N-1][N-1] << "\n";
+
     }
+    
 
 
+    return 0;
 }
