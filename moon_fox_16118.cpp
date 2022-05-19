0d3f88e6e72827e7b1a77f0ca603159fde74acb2
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-05-19 22:19:54 +0900

    2022-05-19
    2022-05-19

--- moon_fox_16118.cpp
+++ moon_fox_16118.cpp
@@ -8,67 +8,82 @@ using namespace std;
 int N,M;
 int INF = INT_MAX;
 vector< pair<int,int> > map[4002];
-double fox_result[4002];
-double wolf_result[4002];
+int fox_result[4002];
+double wolf_result[4002][2];
 
-void dijkstra(double* result, int gubn) {
+void dijkstra_fox() {
     
     for (int i = 0; i <= N; i++) {
-        result[i] = INF;
+        fox_result[i] = INF;
     }
+
+    priority_queue< pair<int,int> > pq_fox;
+    pq_fox.push(make_pair(0,1));
+    fox_result[1] = 0;
+
+    while(!pq_fox.empty()) {
+        int cur_u = pq_fox.top().second;
+        int cur_d = -pq_fox.top().first;
     
-    if (gubn == 0) {
-        priority_queue< pair<int,int> > pq_fox;
-        pq_fox.push(make_pair(0,1));
-        result[1] = 0;
-
-        while(!pq_fox.empty()) {
-            int cur_u = pq_fox.top().second;
-            int cur_d = -pq_fox.top().first;
-      
-            pq_fox.pop();
-      
-            if (result[cur_u] < cur_d) continue;
-      
-            for (int i = 0; i < map[cur_u].size(); i++) {
-                int next_u = map[cur_u][i].first;
-                int next_d = map[cur_u][i].second + cur_d;
-          
-                if (result[next_u] > next_d) {
-                    result[next_u] = next_d;
-                    pq_fox.push(make_pair(-next_d,next_u));
-                }
-            }
-        } 
-    } else {
-        priority_queue< pair< double, pair<int,int> > > pq_wolf;
-        pq_wolf.push(make_pair(0,make_pair(1,1)));
-        result[1] = 0;
-
-        while(!pq_wolf.empty()) {
-            int cur_u = pq_wolf.top().second.first;
-            int cur_v = pq_wolf.top().second.second;
-            double cur_d = -pq_wolf.top().first;
-
-            pq_wolf.pop();
-
-            if (result[cur_u] < cur_d) continue;
-
-            for (int i = 0; i < map[cur_u].size(); i++) {
-                int next_u = map[cur_u][i].first;
-                double next_d; 
-                if (cur_v == 1) next_d = (map[cur_u][i].second * 0.5) + cur_d;
-                else next_d = (map[cur_u][i].second * 2) + cur_d;
-
-                if (result[next_u] > next_d) {
-                    result[next_u] = next_d;
-                    pq_wolf.push(make_pair(-next_d,make_pair(next_u,cur_v*-1)));
-                }
+        pq_fox.pop();
+    
+        if (fox_result[cur_u] < cur_d) continue;
+    
+        for (int i = 0; i < map[cur_u].size(); i++) {
+            int next_u = map[cur_u][i].first;
+            int next_d = map[cur_u][i].second + cur_d;
+        
+            if (fox_result[next_u] > next_d) {
+                fox_result[next_u] = next_d;
+                pq_fox.push(make_pair(-next_d,next_u));
             }
         }
-    }    
+    }     
 }
 
+void dijkstra_wolf() {
+    
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j < 2; j++) {
+            wolf_result[i][j] = INF;
+        }
+    }
+    
+    priority_queue< pair< double, pair<int,int> > > pq_wolf;
+    pq_wolf.push(make_pair(0,make_pair(1,0)));
+    wolf_result[1][0] = 0;
+
+    while(!pq_wolf.empty()) {
+        int cur_u = pq_wolf.top().second.first;
+        int cur_v = pq_wolf.top().second.second;
+        double cur_d = -pq_wolf.top().first;
+
+        pq_wolf.pop();
+
+        if (wolf_result[cur_u][cur_v] < cur_d) continue;
+
+        for (int i = 0; i < map[cur_u].size(); i++) {
+            int next_u = map[cur_u][i].first;
+            double next_d; 
+            int next_v;
+
+            if (cur_v == 0) {
+                next_d = (map[cur_u][i].second * 0.5) + cur_d;
+                next_v = 1;
+            }
+            else {
+                next_d = (map[cur_u][i].second * 2) + cur_d;
+                next_v = 0;
+            } 
+            
+            if (wolf_result[next_u][next_v] > next_d) {
+                wolf_result[next_u][next_v] = next_d;
+                if (cur_v == 0) pq_wolf.push(make_pair(-next_d,make_pair(next_u,1)));
+                else pq_wolf.push(make_pair(-next_d,make_pair(next_u,0)));
+            }
+        }
+    }
+}
 
 int main()
 {   
@@ -87,26 +102,16 @@ int main()
         map[v].push_back(make_pair(u,d));
     }
         
-    dijkstra(fox_result,0);
-    dijkstra(wolf_result,1);
+    dijkstra_fox();
+    dijkstra_wolf();
+
+    int fox_win = 0;
 
     for (int i = 0; i <= N; i++) {
-        cout << fox_result[i] << " ";
+        if (fox_result[i] < wolf_result[i][0] && fox_result[i] < wolf_result[i][1]) fox_win++;
     }
-
-    cout << "\n";
-
-    for (int i = 0; i <= N; i++) {
-        cout << wolf_result[i] << " ";
-    }
-
-    //int fox_win = 0;
-
-    // for (int i = 0; i <= N; i++) {
-    //     if (fox_result[i] < wolf_result[i]) fox_win++;
-    // }
     
-    // cout << fox_win;
+    cout << fox_win;
     
     return 0;
 }
