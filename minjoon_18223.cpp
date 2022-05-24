efdf185c8b387a7252fc64687263f3533449bcae
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-05-24 22:57:01 +0900

    2022-05-24
    2022-05-24

--- minjoon_18223.cpp
+++ minjoon_18223.cpp
@@ -3,46 +3,45 @@
 #include <vector>
 #include <climits>
 
+typedef long long ll;
+
 using namespace std;
 
 int V,E,P;
 vector < pair<int,int> > map[5002];
 int INF = INT_MAX;
-int val_matrix[5002][2];
+int min_roots[5002];
 
-void dijkstra() {
+int dijkstra(int stp,int endp) {
     
     for (int i = 0; i <= V; i++) {
-        for (int j = 0; j < 2; j++) {
-            val_matrix[i][j] = INF;
-        }
+        min_roots[i] = INF;
     }
     
-    priority_queue< pair< int, pair<int,int> > > pq;
-    pq.push(make_pair(0,make_pair(1,0)));
-    val_matrix[1][0] = 0;
+    priority_queue< pair<int,int> > pq;
+    pq.push(make_pair(0,stp));
+    min_roots[stp] = 0;
     
     while(!pq.empty()) {
-        int cur_v = pq.top().second.first;
+        int cur_v = pq.top().second;
         int cur_d = -pq.top().first;
-        int cur_p = pq.top().second.second;
-        
+
         pq.pop();
         
-        if (val_matrix[cur_v][cur_p] < cur_d) continue;
+        if (min_roots[cur_v] < cur_d) continue;
         
         for (int i = 0; i < map[cur_v].size(); i++) {
             int next_v = map[cur_v][i].first;
             int next_d = map[cur_v][i].second + cur_d;
-            int next_p = cur_p;
-            
-            if (next_v == P) next_p = 1;
-            if (val_matrix[next_v][next_p] > next_d) {
-                val_matrix[next_v][next_p] = next_d;
-                pq.push(make_pair(-next_d,make_pair(next_v,next_p)));
+    
+            if (min_roots[next_v] > next_d) {
+                min_roots[next_v] = next_d;
+                pq.push(make_pair(-next_d,next_v));
             }
         }
     }
+    
+    return min_roots[endp];
 }
 
 int main()
@@ -59,14 +58,15 @@ int main()
         map[v].push_back(make_pair(u,d));
     }
     
-    dijkstra();
+    int min_root = dijkstra(1,V);
+    int f_root_f = dijkstra(1,P);
+    int f_root_s = dijkstra(P,V);
     
-    if (val_matrix[V][0] < val_matrix[V][1]) cout << "GOOD BYE";
+    if (min_root < f_root_f + f_root_s) cout << "GOOD BYE";
     else cout << "SAVE HIM";
     
     
     return 0;
 }
-
 
 
