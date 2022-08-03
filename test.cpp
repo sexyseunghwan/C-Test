c350b2c2e52f2572b564ee52631cded9d680f7d8
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-08-03 23:53:46 +0900

    2022-08-03
    2022-08-03

--- test.cpp
+++ test.cpp
@@ -5,74 +5,84 @@
 
 using namespace std;
 
-int INF = INT_MAX;
-int T,N,M,K;
-int ver_val[102][10002];
-vector < pair< pair<int,int>,int> > map[102];
+int matrix[101][101];
+int visit[101][101];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+int N;
 
-void dijkstra() {
-    
-    for (int i = 0; i <= N; i++) {
-        for (int j = 0; j <= M; j++) {
-            ver_val[i][j] = INF;
-        }
+void dfs(int x, int y, int min_val)
+{
+    visit[x][y] = 1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+
+        if (new_x < N && new_y < N && new_x >= 0 && new_y >= 0 && matrix[new_x][new_y] > min_val && visit[new_x][new_y] == 0)
+            dfs(new_x, new_y, min_val);
     }
-    
-   priority_queue< pair< pair<int,int> ,int> > pq;
-   pq.push(make_pair(make_pair(0,0),1));
-   ver_val[1][0] = 0;
-   
-   while(!pq.empty()) {
-       int cur_u = pq.top().second;
-       int cur_d = -pq.top().first.first;
-       int cur_c = -pq.top().first.second;
-       
-       pq.pop();
-       
-       for (int i = 0; i < map[cur_u].size(); i++) {
-            int new_u = map[cur_u][i].second;
-            int new_d = map[cur_u][i].first.first + cur_d;
-            int new_c = map[cur_u][i].first.second + cur_c;
-            
-            if (new_c > M) continue;
-            else if (ver_val[new_u][new_c] > new_d) {
-                ver_val[new_u][new_c] = new_d;
-                pq.push(make_pair(make_pair(-new_d,-new_c),new_u));
-            }
-            
-       }
-   }
 }
 
 int main()
 {
-    cin >> T;
-    
-    while(T--) {
-        cin >> N >> M >> K;
-        
-        for (int i = 0; i <= N; i++) {
-            map[i].clear();
+    int min_val = 101;
+    int max_val = 0;
+    int max_count = 0;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int input_num;
+            cin >> input_num;
+            min_val = min_val > input_num ? input_num : min_val;
+            max_val = input_num > max_val ? input_num : max_val;
+            matrix[i][j] = input_num;
         }
-        
-        for (int i = 0; i < K; i++) {
-            int u,v,c,d;
-            cin >> u >> v >> c >> d;
-            
-            map[u].push_back(make_pair(make_pair(d,c),v));
-        }
-        
-        dijkstra();
-        
-        int result = INF;
-        
-        for (int i = 0; i <= M; i++) {
-            result = min(result,ver_val[N][i]);
-        }
-        
-        if (result == INF) cout << "Poor KCM\n";
-        else cout << result << "\n";
     }
-    
+
+    for (int s = min_val; s <= max_val; s++)
+    {
+        int count = 0;
+
+        for (int i = 0; i < 101; i++)
+        {
+            for (int j = 0; j < 101; j++)
+            {
+                visit[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (visit[i][j] == 0 && matrix[i][j] > min_val)
+                {
+                    dfs(i, j, s);
+                    count++;
+                }
+            }
+        }
+        cout << "\n";
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                cout << visit[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << count << "\n";
+        max_count = max_count > count ? max_count : count;
+    }
+
+    cout << max_count;
+
     return 0;
 }
