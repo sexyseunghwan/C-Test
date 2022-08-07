49110b7d47aba235ddf41a85124decde13152b61
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-08-07 20:40:50 +0900

    2022-08-40
    2022-08-40

--- test.cpp
+++ test.cpp
@@ -13,76 +13,12 @@ int N;
 
 void dfs(int x, int y, int min_val)
 {
-    visit[x][y] = 1;
-
-    for (int i = 0; i < 4; i++)
-    {
-        int new_x = x + dx[i];
-        int new_y = y + dy[i];
-
-        if (new_x < N && new_y < N && new_x >= 0 && new_y >= 0 && matrix[new_x][new_y] > min_val && visit[new_x][new_y] == 0)
-            dfs(new_x, new_y, min_val);
-    }
 }
 
 int main()
 {
-    int min_val = 101;
-    int max_val = 0;
-    int max_count = 0;
 
-    cin >> N;
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            int input_num;
-            cin >> input_num;
-            min_val = min_val > input_num ? input_num : min_val;
-            max_val = input_num > max_val ? input_num : max_val;
-            matrix[i][j] = input_num;
-        }
-    }
-
-    for (int s = min_val; s <= max_val; s++)
-    {
-        int count = 0;
-
-        for (int i = 0; i < 101; i++)
-        {
-            for (int j = 0; j < 101; j++)
-            {
-                visit[i][j] = 0;
-            }
-        }
-
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (visit[i][j] == 0 && matrix[i][j] > min_val)
-                {
-                    dfs(i, j, s);
-                    count++;
-                }
-            }
-        }
-        cout << "\n";
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                cout << visit[i][j] << " ";
-            }
-            cout << "\n";
-        }
-
-        cout << count << "\n";
-        max_count = max_count > count ? max_count : count;
-    }
-
-    cout << max_count;
+    // cout << max_count;
 
     return 0;
 }
