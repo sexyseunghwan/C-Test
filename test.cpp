cfdee1a501f50409f3aeeaa503f98c216ff93be1
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-08-30 15:46:48 +0900

    2022-08-30
    2022-08-30

--- test.cpp
+++ test.cpp
@@ -1,75 +1,48 @@
 #include <iostream>
 #include <climits>
-#include <algorithm>
 #include <cstring>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 using namespace std;
 
-void dfs(const char* str) {
+int N;
+int map[501][501];
+int dp[501][501];
+int dr[4] = {1,-1,0,0};
+int dc[4] = {0,0,1,-1};
+int max_moving;
 
-     cout<<str<<endl;
+int dynamic_prog(int r, int c) {
+
+    if (dp[r][c] != 0) return dp[r][c];
+    dp[r][c] = 1;
+    
+    for (int i = 0; i < 4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+
+        if (nr >= 0 && nc >= 0 && nr < N && nc < N && map[nr][nc] > map[r][c]) {
+            dp[r][c] = max(dp[r][c],dynamic_prog(nr,nc) + 1);
+        }
+    }
+
+    return dp[r][c];
 }
-
-
 
 
 int main()
 {   
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
     
+    cin >> N;
+
+    for (int i = 0; i < N*N; i++) cin >> map[i/N][i%N];
+
+    for (int i = 0; i < N*N; i++) {
+        max_moving = max(max_moving,dynamic_prog(i/N,i%N));    
+    }    
     
+    cout << max_moving;
 
-    string a = "trds\n";
-    cout << a << endl;
-    a.pop_back();
-    cout << a << endl;
-    //a.erase(remove(a.begin(),a.end(),'\n'));
-
-    //path.erase(remove(path.begin(),path.end(),'\n'))
-
-    //cout<<a;
-    //int b = 13;
-
-    //cout<<a+to_string(b)<<endl;
-    
-    //string c = stoi(b);
-    
-    //cout << a+c << endl;
-    //cout<<b+10<<endl;
-
-    //string a = "lsdfkja;lsdkfj;alksdjf;laksdjf";
-    //const char *str = a;
-    //const char *str = "lsdfkja;lsdkfj;alksdjf;laksdjf";
-    //char ss[100] = "asd";
-    //ss[3] = 'k';
-    
-    //cout<<ss<<endl;
-
-    //const char *chr = ss;
-
-    //dfs(chr);
-
-    //string str = "hello";
-	
-    //string str2 = "word";
-
-    //cout<<str+str2<<endl;
-    
-    //char* ch = new char[str.length() + 1];
-	//ch[str.length()] = '\0';
-	//copy(str.begin(), str.end(), ch);
-
-    //cout<<ch<<endl;
-
-
-
-    //cout<<str<<endl;
-    //cout<<result(strcpy(ch,str.c_str()))<<endl;
-    
-    //dfs(str);
-  
     return 0;
 }
