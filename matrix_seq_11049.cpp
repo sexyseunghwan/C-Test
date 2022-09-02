d0e47324f52451ea43f4c824bc344c715b7460b5
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-02 18:48:23 +0900

    2022-09-02
    2022-09-02

--- matrix_seq_11049.cpp
+++ matrix_seq_11049.cpp
@@ -2,15 +2,27 @@
 
 using namespace std;
 
-
+int N;
+int matrix[501][501];
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
+    
+    cin >> N;
+    
+    for (int i = 1; i < N; i++) {
+        for (int dx = 1; dx + i <= N; dx++) {
+            int dy = dx + i;
+            for (int mid = dx; mid < dy; mid++) {
+                printf("i : %d, dx : %d, dy : %d, mid : %d\n",i,dx,dy,mid);
+            }
+        }
+    }
+
 
     
-
 
     return 0;
 }
