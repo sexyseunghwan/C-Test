2b4dcdd23a3d6e4d1d9f972dc56710b794ada94c
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-08 23:39:26 +0900

    2022-09-08
    2022-09-08

--- test.cpp
+++ test.cpp
@@ -26,29 +26,19 @@ int main()
         pairs[i] = make_pair(r,c);
     }  
     
-    for (int i = 0; i < N; i++) { // 0 1 2
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < N-i; j++) { 
-            // i == 0 : 0 1 2
-            // i == 1 : 0 1
-            // i == 2 : 0
             int k = j;
             int s = i+j+1;
 
             dp[k][s] = MAX;
-            for (int t = 0; t < s; t++) {
+            for (int t = k; t < s; t++) {
                 dp[k][s] = min(dp[k][s],dp[k][t] + dp[t+1][s] + bias(k,t,s));       
             }   
         } 
     }
 
-    // for (int i = 0; i < N; i++) {
-    //     for (int j = 0; j < N; j++) {
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    //cout << dp[0][N-1];    
+    cout << dp[0][N-1];    
     
     
     return 0;
