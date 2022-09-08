88c55488022e4ce703e4fa4be4a10fe4ad1a2b71
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-08 23:27:00 +0900

    2022-09-08
    2022-09-08

--- test.cpp
+++ test.cpp
@@ -5,29 +5,13 @@
 using namespace std;
 
 int N;
-int map[501][501];
+int MAX = INT_MAX;
+pair < int,int > pairs[501];
 int dp[501][501];
-int dr[4] = {1,-1,0,0};
-int dc[4] = {0,0,1,-1};
-int max_moving;
 
-int dynamic_prog(int r, int c) {
-
-    if (dp[r][c] != 0) return dp[r][c];
-    dp[r][c] = 1;
-    
-    for (int i = 0; i < 4; i++) {
-        int nr = r + dr[i];
-        int nc = c + dc[i];
-        
-        if (nr >= 0 && nc >= 0 && nr < N && nc < N && map[nr][nc] > map[r][c]) {
-            dp[r][c] = max(dp[r][c],dynamic_prog(nr,nc) + 1);
-        }
-    }
-
-    return dp[r][c];
+int bias(int k, int t, int s) {
+    return pairs[k].first * pairs[t].second * pairs[s].second;    
 }
-
 
 int main()
 {   
@@ -35,14 +19,38 @@ int main()
     cin.tie(0), cout.tie(0);
     
     cin >> N;
-
-    for (int i = 0; i < N*N; i++) cin >> map[i/N][i%N];
-
-    for (int i = 0; i < N*N; i++) {
-        max_moving = max(max_moving,dynamic_prog(i/N,i%N));    
-    }    
     
-    cout << max_moving;
+    for (int i = 0; i < N; i++) {
+        int r,c;
+        cin >> r >> c;
+        pairs[i] = make_pair(r,c);
+    }  
+    
+    for (int i = 0; i < N; i++) { // 0 1 2
+        for (int j = 0; j < N-i; j++) { 
+            // i == 0 : 0 1 2
+            // i == 1 : 0 1
+            // i == 2 : 0
+            int k = j;
+            int s = i+j+1;
 
+            dp[k][s] = MAX;
+            for (int t = 0; t < s; t++) {
+                dp[k][s] = min(dp[k][s],dp[k][t] + dp[t+1][s] + bias(k,t,s));       
+            }   
+        } 
+    }
+
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < N; j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //cout << dp[0][N-1];    
+    
+    
     return 0;
 }
+
