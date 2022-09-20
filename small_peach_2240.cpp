8f78ab83060c1d2368f88b01e2c20829f2c2dd40
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-20 23:22:45 +0900

    2022-09-20
    2022-09-20

---
+++ small_peach_2240.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int N,K;
+int p_arr[3][1001][32];
+int input_arr[1001];
+int peach_cnt = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> input_arr[i];
+    }
+
+    //for (int i = 1; i <= )
+    
+    
+
+    return 0;
+}
