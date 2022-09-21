40ee1efdabe3048b2bf180523c669768fc66a15b
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-21 18:47:18 +0900

    2022-09-21
    2022-09-21

--- small_peach_2240.cpp
+++ small_peach_2240.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N,K;
-int p_arr[3][1001][32];
+int dp[3][1001][32];
 int input_arr[1001];
 int peach_cnt = 0;
 
@@ -14,12 +14,32 @@ int main()
     
     cin >> N >> K;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         cin >> input_arr[i];
     }
 
-    //for (int i = 1; i <= )
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= K+1; j++) {
+            if (input_arr[i] == 1) {
+                dp[1][i][j] = max(dp[1][i-1][j] + 1, dp[2][i-1][j-1] + 1);
+                dp[2][i][j] = max(dp[1][i-1][j-1], dp[2][i-1][j]);
+            } else {
+                
+                if (i == 1 && j == 1) continue;
+
+                dp[1][i][j] = max(dp[1][i-1][j], dp[2][i-1][j-1]);
+                dp[2][i][j] = max(dp[1][i-1][j-1] + 1, dp[2][i-1][j] + 1);
+            }
+        }
+    }
+
+
+
+    for (int i = 1; i <= K+1; i++) {
+        peach_cnt = max(peach_cnt,max(dp[1][N][i],dp[2][N][i]));
+    }
     
+    cout << peach_cnt;
     
 
     return 0;
