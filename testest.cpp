d4a8b9744428f15b4ccf25b2a52427102cdd9ab1
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-10-03 22:42:27 +0900

    2022-10-03
    2022-10-03

--- testest.cpp
+++ testest.cpp
@@ -2,28 +2,41 @@
 
 using namespace std;
 
-int N,K;
-long dp[201][201];
-long DIV = 1000000000;
+int N;
+int dp[501][501];
+//pair < int,int > pairs[501];
+int use_case[501];
+
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     
-    cin >> N >> K;
+    cin >> N;
 
-    for (int i = 1; i <= K; i++) dp[i][0] = 1;
-    for (int i = 0; i <= N; i++) dp[1][i] = 1;
+    //모든 dp 배열을 최대로 지정해준다.
+
+    for (int i = 0; i < N; i++) {
+        int r,c;
+        cin >> r >> c;
+        
+        if (i==0) {
+            use_case[0] = r;
+            use_case[1] = c;
+        } else {
+            use_case[i+1] = c;
+        } 
+    }
     
-    for (int i = 1; i <= K; i++) {
-        for (int j = 1; j <= N; j++) {
-            long sum = dp[i][j-1] + dp[i-1][j];
-            dp[i][j] = sum % DIV;
+    for (int i = 0; i < N-1; i++) {
+        for (int j = 0; j < N-i-1; j++) {
+            for (int k = 0; k < i+1; k++) {
+                //dp[j][j+1] = dp[][] + dp[][] + use_case[]*use_case[]*use_case[];
+            }
         }
     }
 
-    cout << dp[K][N];
-
+    
     return 0;
 }
